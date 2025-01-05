13c66b0a35a422fc1abbe07a0182d51b35de8511
Jaiganesh <jaiganeshcse4@gmail.com>
2025-01-05 17:00:20 +0530

    Two file updated

--- Text1.jsx
+++ Text1.jsx
@@ -1,1 +1,10 @@
-comments from Text1.jsx
+import React from 'react'
+
+function Text1() {
+  return (
+    <div>comments from Text1.jsx
+</div>
+  )
+}
+
+export default Text1
---
+++ Text2.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Text2() {
+  return (
+    <div>comments from Text2.jsx
+</div>
+  )
+}
+
+export default Text2
